f52d877f721679259771b2628f3ff426efb2c093
Christian Ebeling <Christian.Ebeling@SCAI.Fraunhofer.de>
2023-10-05 08:18:20 +0200

    added Node and Edge to __init__

--- neo4j_tools/__init__.py
+++ neo4j_tools/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Christian.Ebeling"""
 __email__ = 'Christian.Ebeling@SCAI.Fraunhofer.de'
 __version__ = '0.1.0'
 
-from neo4j_tools.neo4j_tools import Db
+from neo4j_tools.neo4j_tools import Db, Node, Edge
